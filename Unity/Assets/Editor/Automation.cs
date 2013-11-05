f7a82667743d151aa2fe2e90bb3d4885258ec6b5
Shawn Adams <shawn.henry.adams@gmail.com>
2013-11-05 07:32:19 -0800

    Added main script to scene

---
+++ Unity/Assets/Editor/Automation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Automation {
+
+	public static void Build() {
+		string[] scenes = { "Assets/Scenes/Menu.unity" };
+		var buildInfo = BuildPipeline.BuildPlayer(scenes, string.Format("../Builds/ChangeGame{0}.apk", System.DateTime.Now.ToFileTimeUtc()), BuildTarget.Android, BuildOptions.None);
+		EditorApplication.Exit(string.IsNullOrEmpty(buildInfo)? 0 : 1);
+	}
+	
+}
