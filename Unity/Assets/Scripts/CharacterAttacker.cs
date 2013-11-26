f247fb366fab62edd0eaeeb910ad04b3095c1be6
Shawn Adams <shawn.henry.adams@gmail.com>
2013-11-26 08:11:04 -0800

    More generic refactored names of hit variables

--- Unity/Assets/Scripts/CharacterAttacker.cs
+++ Unity/Assets/Scripts/CharacterAttacker.cs
@@ -5,15 +5,15 @@ public class CharacterAttacker : MonoBehaviour {
 
 	public LayerMask targetLayerMask;
 
-	public float punchHitRadius = 0.1f;
-	public Vector3 punchHitOffset = new Vector3(0.1f, 0.4f, 0f);
+	public float hitRadius = 0.1f;
+	public Vector3 hitOffset = new Vector3(0.1f, 0.4f, 0f);
 	
 	public void Attack() {
-		var directedPunchOffset = new Vector3(punchHitOffset.x * Mathf.Sign(transform.localScale.x), punchHitOffset.y);
+		var directedPunchOffset = new Vector3(hitOffset.x * Mathf.Sign(transform.localScale.x), hitOffset.y);
 		//Debug.DrawLine(transform.position + directedPunchOffset - Vector3.right * punchHitRadius, transform.position + directedPunchOffset + Vector3.right * punchHitRadius);
 		//Debug.DrawLine(transform.position + directedPunchOffset - Vector3.up * punchHitRadius, transform.position + directedPunchOffset + Vector3.up * punchHitRadius);
 		//Debug.Break();
-		var colliders = Physics2D.OverlapCircleAll(transform.position + directedPunchOffset, punchHitRadius, targetLayerMask.value);
+		var colliders = Physics2D.OverlapCircleAll(transform.position + directedPunchOffset, hitRadius, targetLayerMask.value);
 		foreach(var collider in colliders) {
 			var damageable = collider.GetComponentInChildren(typeof(IDamageable)) as IDamageable;
 			if(damageable == null) {
